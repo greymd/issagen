331e48c7f63321e71f9a7a1376e160860e338b25
Yasuhiro Yamada <yamadagrep@gmail.com>
2020-05-24 16:17:23 +0100

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,5 +3,5 @@ set -ue
 trap 'rm -f $THIS_DIR/issagen.tar.gz' EXIT
 readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" && pwd)"
 tar zcvf "${THIS_DIR}"/issagen.tar.gz -C "${THIS_DIR}" bin .tar2package.yml
-docker run -i greymd/tar2rpm < "${THIS_DIR}"/issagen.tar.gz > "${THIS_DIR}/pkg/issagen.rpm"
-docker run -i greymd/tar2deb < "${THIS_DIR}"/issagen.tar.gz > "${THIS_DIR}/pkg/issagen.deb"
+docker run -i greymd/tar2rpm:1.0.1 < "${THIS_DIR}"/issagen.tar.gz > "${THIS_DIR}/pkg/issagen.rpm"
+docker run -i greymd/tar2deb:1.0.1 < "${THIS_DIR}"/issagen.tar.gz > "${THIS_DIR}/pkg/issagen.deb"
